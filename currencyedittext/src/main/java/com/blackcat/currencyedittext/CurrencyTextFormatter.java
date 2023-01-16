b3b19ca81a77774be867f92ab5cbc0c1609ba62d
oguzkuka <oguz@kukapps.com>
2023-01-16 10:00:51 +0300

    library Update

--- currencyedittext/src/main/java/com/blackcat/currencyedittext/CurrencyTextFormatter.java
+++ currencyedittext/src/main/java/com/blackcat/currencyedittext/CurrencyTextFormatter.java
@@ -9,25 +9,25 @@ import java.util.Locale;
 
 public final class CurrencyTextFormatter {
 
-    private CurrencyTextFormatter(){}
+    private CurrencyTextFormatter() {
+    }
 
-    public static String formatText(String val, Locale locale){
+    public static String formatText(String val, Locale locale) {
         return formatText(val, locale, Locale.US, null);
     }
 
-    public static String formatText(String val, Locale locale, Locale defaultLocale){
+    public static String formatText(String val, Locale locale, Locale defaultLocale) {
         return formatText(val, locale, defaultLocale, null);
     }
 
-    public static String formatText(String val, Locale locale, Locale defaultLocale, Integer decimalDigits){
+    public static String formatText(String val, Locale locale, Locale defaultLocale, Integer decimalDigits) {
         //special case for the start of a negative number
-        if(val.equals("-")) return val;
+        if (val.equals("-")) return val;
 
         int currencyDecimalDigits;
-        if (decimalDigits != null){
+        if (decimalDigits != null) {
             currencyDecimalDigits = decimalDigits;
-        }
-        else {
+        } else {
             Currency currency = Currency.getInstance(locale);
             try {
                 currencyDecimalDigits = currency.getDefaultFractionDigits();
@@ -44,8 +44,7 @@ public final class CurrencyTextFormatter {
             try {
                 Log.e("CurrencyTextFormatter", "Error detected for locale: " + locale + ", falling back to default value: " + defaultLocale);
                 currencyFormatter = (DecimalFormat) DecimalFormat.getCurrencyInstance(defaultLocale);
-            }
-            catch(Exception e1){
+            } catch (Exception e1) {
                 Log.e("CurrencyTextFormatter", "Error detected for defaultLocale: " + defaultLocale + ", falling back to USD.");
                 currencyFormatter = (DecimalFormat) DecimalFormat.getCurrencyInstance(Locale.US);
             }
@@ -53,17 +52,17 @@ public final class CurrencyTextFormatter {
 
         //retain information about the negativity of the value before stripping all non-digits
         boolean isNegative = false;
-        if (val.contains("-")){
+        if (val.contains("-")) {
             isNegative = true;
         }
 
         //strip all non-digits so the formatter always has a 'clean slate' of numbers to work with
         val = val.replaceAll("[^\\d]", "");
         //if there's nothing left, that means we were handed an empty string. Also, cap the raw input so the formatter doesn't break.
-        if(!val.equals("")) {
+        if (!val.equals("")) {
 
             //if we're given a value that's smaller than our decimal location, pad the value.
-            if (val.length() <= currencyDecimalDigits){
+            if (val.length() <= currencyDecimalDigits) {
                 String formatString = "%" + currencyDecimalDigits + "s";
                 val = String.format(formatString, val).replace(' ', '0');
             }
@@ -80,9 +79,8 @@ public final class CurrencyTextFormatter {
 
             //finally, do the actual formatting
             currencyFormatter.setMinimumFractionDigits(currencyDecimalDigits);
-            val = currencyFormatter.format(newTextValue);
-        }
-        else {
+            val = String.valueOf(newTextValue);
+        } else {
             throw new IllegalArgumentException("Invalid amount of digits found (either zero or too many) in argument val");
         }
         return val;
